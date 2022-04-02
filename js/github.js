33e2a836af2e7cdd1e0970c284822c4e109f3caf
shallx <rafat.rashid247@gmail.com>
2022-04-02 18:56:14 +0600

    Add origin page added

--- js/github.js
+++ js/github.js
@@ -4,6 +4,14 @@ $("#generateCloneLink").click(function () {
     sshLink = sshLink.replace("github.com", `github.com-${username}`);
     $("#output").val("git clone " + sshLink);
   });
+
+
+  $("#generateAddOriginCommand").click(function () {
+    var sshLink = $("#sshLink").val();
+    var username = $("#username").val();
+    sshLink = sshLink.replace("github.com", `github.com-${username}`);
+    $("#output").val("git remote add origin " + sshLink);
+  });
   
   // clear button
   $("#clear").click(function () {
